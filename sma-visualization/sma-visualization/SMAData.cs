6cad7d1300a42f15d8a271ae8ff7c1f3bdd8d76f
Vanja Šerfeze <serfezev@gmail.com>
2022-04-22 18:18:51 +0200

    napravljene klase

--- sma-visualization/sma-visualization/SMAData.cs
+++ sma-visualization/sma-visualization/SMAData.cs
@@ -8,5 +8,36 @@ namespace sma_visualization
 {
     internal class SMAData
     {
+        public string Symbol { get; set; }
+        public string Indicator { get; set; }
+        public string LastRefresh { get; set; }
+        public string Interval { get; set; }
+        public string TimePeriod { get; set; }
+        public string SeriesType { get; set; }
+        public string TimeZone { get; set; }
+        public List<SMA> SMAlist { get; set; }
+        public SMAData(string symbol, string indicator, string lastRefresh, string interval, string timePeriod, string seriesType, string timeZone, List<SMA> smalist)
+        {
+            Symbol = symbol;
+            Indicator = indicator;
+            Interval = interval;
+            TimePeriod = timePeriod;
+            SeriesType = seriesType;
+            TimeZone = timeZone;
+            SMAlist = smalist;
+
+        }
     }
-}
+
+        internal class SMA
+        {
+            public DateTime Date { get; set; }
+            public int Value { get; set; }
+
+            public SMA(DateTime date, int v)
+            {
+                Date = date;
+                Value = v;
+            }
+        }
+    }
